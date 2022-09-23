220f9968348ceac37f6583f9895c2560cab8818a
Shiba Kumar Palo <69417069+SHivaKUmar212001@users.noreply.github.com>
2022-09-23 12:17:39 +0530

    initial commit

---
+++ UnityProject/Assets/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 playerCoordinates;
+    public static Player Instance;
+    RectTransform rectTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rectTransform.anchoredPosition = playerCoordinates * new Vector2(1,-1);
+    }
+}
---
+++ UnityProject/Assets/Scripts/ClientFastImg.cs
@@ -0,0 +1,84 @@
+﻿using AsyncIO;
+using NetMQ;
+using NetMQ.Sockets;
+using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReceiverFastImg
+{
+    private readonly Thread receiveThread;
+    private bool running;
+
+    public ReceiverFastImg()
+    {
+        receiveThread = new Thread((object callback) =>
+        {
+            using (var socket = new PullSocket())
+            {
+                socket.Connect("tcp://localhost:5555");
+
+                while (running)
+                {
+                    byte[] rawImage = socket.ReceiveFrameBytes();
+                    ((Action<byte[]>)callback)(rawImage);
+                }
+            }
+        });
+    }
+
+    public void Start(Action<byte[]> callback)
+    {
+        running = true;
+        receiveThread.Start(callback);
+    }
+
+    public void Stop()
+    {
+        running = false;
+        receiveThread.Join();
+    }
+}
+
+public class ClientFastImg : MonoBehaviour
+{
+    private readonly ConcurrentQueue<Action> runOnMainThread = new ConcurrentQueue<Action>();
+    private ReceiverFastImg receiver;
+    private Texture2D tex;
+    public RawImage image;
+
+    public void Start()
+    {
+        tex = new Texture2D(960, 720, TextureFormat.RGB24, mipChain: false);
+        image.texture = tex;
+
+        ForceDotNet.Force();
+        receiver = new ReceiverFastImg();
+        receiver.Start((byte[] rawImage) => runOnMainThread.Enqueue(() =>
+            {
+                tex.LoadRawTextureData(rawImage);
+                tex.Apply(updateMipmaps: false);
+            }
+        ));
+    }
+
+    public void Update()
+    {
+        if (!runOnMainThread.IsEmpty)
+        {
+            Action action;
+            while (runOnMainThread.TryDequeue(out action))
+            {
+                action.Invoke();
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        receiver.Stop();
+        NetMQConfig.Cleanup();
+    }
+}
